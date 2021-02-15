28bc4a96eee7198351017245a50b09913e170402
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-02-15 18:12:50 -0500

    getting choices desplaced as buttons hopefully

--- Assets/Scripts/ScriptableObjects/Question.cs
+++ Assets/Scripts/ScriptableObjects/Question.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Choice
+{
+
+    //a refrence to who is speaking
+    [TextArea(2, 5)]
+    public string text;
+    public Dialog dialog;
+}
 public class Question : MonoBehaviour
 {
-    [System.Serializable]
-    public struct Choice
-    {
-        
-        //a refrence to who is speaking
-        [TextArea(2, 5)]
-        public string text;
-        public Dialog dialog;
-    }
+    
 
     [CreateAssetMenu(fileName = "New Question", menuName = "Question")]
     public class Dialog: ScriptableObject
