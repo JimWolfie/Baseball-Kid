36170779e8de8773f2f8486eddbfdf89bb4b522d
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-02-21 22:04:38 -0500

    made the void event again. i should just save this script system because its so nice for prototyping

---
+++ Assets/Scripts/EventSystem/VoidEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOEvents
+{
+    [CreateAssetMenu(fileName = "new void event", menuName ="GameEvent/void")]
+    public class VoidEvent: BaseGameEvent<Void>
+    {
+        public void Raise()=> Raise(new Void());
+    }
+}
+
