f17f5cb0fcd2d40ac7632bc7aefb81fb4d7b926c
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-04-15 01:43:25 -0400

    prefab version

---
+++ Assets/Scenes/Levels/SpawnMangers/testing_container.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Spawnaeebles", menuName = "thingie")]
+public class testing_container : ScriptableObject
+{
+
+    [SerializeField] public List<EnemySpawn> enemyListering;
+
+[Serializable]
+public class EnemySpawn
+{
+    [SerializeField]public GameObject enemy;
+    public float x;
+    public float y;
+
+}
+}
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Spawnablesx _enemyList;
-    
+   
     public List<EnemySpawnPoint> remaining;
-
-    
+    public int totalE;
+    public float delay_between_waves;
+    public int _waveCount;
+    public string Next_Scene_By_Name;
 
     private void Awake()
     {
         
     }
+    private void Start()
+    {
+        LoadRemaining();
+    }
 
-    public void loadRemaining()
+    public void LoadRemaining()
     {
         var ee = _enemyList;
+        foreach(EnemySpawnPoint e in ee.enemyArray)
+        {
+            
+            remaining.Add(e);
+        }
+        totalE = remaining.Count;
+
+
+    }
+
+    void spawnNew(float x, float y)
+    {
+        GameObject g = new GameObject();
+        g.transform.position = new Vector2(x, y);
         
     }
 }
