65e3ca0ab92c4184f03ecdaa8f536d6db8314133
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-04-15 00:12:42 -0400

    bbleh

---
+++ Assets/Scenes/Levels/SpawnMangers/Spawnablesx.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spawnablesx", menuName = "enemy data Containers")]
+public class Spawnablesx : ScriptableObject
+{
+    public List<string> enemyArray;
+
+    
+   
+}
+
