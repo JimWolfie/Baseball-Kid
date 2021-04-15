e7b8201d0271b040d6b810d77d04e38ec61eba44
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-04-15 00:44:29 -0400

    push

--- Assets/Scenes/Levels/SpawnMangers/Spawnablesx.cs
+++ Assets/Scenes/Levels/SpawnMangers/Spawnablesx.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Spawnablesx", menuName = "enemy data Containers")]
 public class Spawnablesx : ScriptableObject
 {
-    public List<string> enemyArray;
+    public List<EnemySpawnPoint> enemyArray;
 
     
    
+}
+[Serializable]
+public struct EnemySpawnPoint
+{
+    public string Name;
+    public float x;
+    public float y;
+
 }
 
